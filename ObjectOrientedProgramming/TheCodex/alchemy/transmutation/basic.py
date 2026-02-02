f2eff34d378e8a9f58718adb6111c25aafb3d296
Matan Aziza <maziza@c1r14s3.marseille.42.school>
2026-02-02 15:53:18 +0100

    flake8 TheCodex

--- ObjectOrientedProgramming/TheCodex/alchemy/transmutation/basic.py
+++ ObjectOrientedProgramming/TheCodex/alchemy/transmutation/basic.py
@@ -1,8 +1,10 @@
 from alchemy.elements import create_fire, create_earth
 
+
 def lead_to_gold():
     f = create_fire()
     return f"Lead transmuted to gold using {f}"
+
 
 def stone_to_gem():
     e = create_earth()
