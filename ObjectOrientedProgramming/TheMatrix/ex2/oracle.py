5df029a4eeee7e683066f20d208964efc66ca139
MatanAziza <matanaziza@gmail.com>
2026-02-13 18:04:51 +0100

    ex2 matrix

--- ObjectOrientedProgramming/TheMatrix/ex2/oracle.py
+++ ObjectOrientedProgramming/TheMatrix/ex2/oracle.py
@@ -1,8 +1,6 @@
 import sys
 import os
 
-from dotenv import dotenv_values
-
 
 if __name__ == "__main__":
     in_venv = sys.base_prefix != sys.prefix
@@ -24,10 +22,15 @@ if __name__ == "__main__":
             "LOG_LEVEL": "Log level",
             "ZION_ENDPOINT": "Zion Network"
                     }
+        mode = ["development", "production"]
         env = dict()
+        print("ORACLE STATUS: Reading the Matrix...\n\n")
         print("Configuration loaded:")
         for key, value in os.environ.items():
             if key in keys.keys():
+                if key == "MATRIX_MODE":
+                    if value not in mode:
+                        value = "developement"
                 if key == "API_KEY":
                     if value == "secret123":
                         value = "Authenticated"
@@ -42,5 +45,25 @@ if __name__ == "__main__":
                     if env.get("Database") == "Connected to local instance":
                         value = "Online"
                 env.update({keys.get(key): value})
-        for value in keys.values():
-            print(f"{value}: {env.get(value)}")
+        no_var = "Not present in the env file."
+        for key in keys.values():
+            if key not in env.keys() and key != "API Access":
+                env.update({key: no_var})
+        if no_var in env.values():
+            print("Missing configuration variable:")
+            for key, value in keys.items():
+                if env.get(value) == no_var:
+                    print(f"- {key}")
+        else:
+            for value in keys.values():
+                print(f"{value}: {env.get(value)}")
+            print("\nEnvironment security check:")
+            print(
+                "[OK] No hardcoded secrets detected\n"
+                "[OK] .env file properly configured"
+                    )
+            if env.get("Mode") == "development":
+                print("[OK] Production override available")
+            else:
+                print("[OK] Default variable 'Development' available")
+            print("\nThe Oracle sees all configurations.")
