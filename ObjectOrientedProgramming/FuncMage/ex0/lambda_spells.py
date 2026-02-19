1081876f4f4c9a9e2f2eb802aea2ff3a9c6ccf0d
MatanAziza <matanaziza@gmail.com>
2026-02-19 19:52:32 +0100

    ex0 end

--- ObjectOrientedProgramming/FuncMage/ex0/lambda_spells.py
+++ ObjectOrientedProgramming/FuncMage/ex0/lambda_spells.py
@@ -1,3 +1,40 @@
 def artifact_sorter(artifacts: list[dict]) -> list[dict]:
-    new_list = sorted(artifacts, key=lambda x : x["power"], reverse=True)
-    return new_list
+    res = sorted(artifacts, key=lambda x : x["power"], reverse=True)
+    return res
+
+def power_filter(mages: list[dict], min_power: int) -> list[dict]:
+    res = filter(lambda x: x["power"] > min_power, mages)
+    return list(res)
+
+def spell_tranformer(spells: list[str]) -> list[str]:
+    res = map(lambda s: f"*{s}*", spells)
+    return list(res)
+
+def mage_stats(mages: list[dict]) -> dict:
+    max_power = max(mages, key=lambda x: x["power"])["power"]
+    min_power = min(mages, key=lambda x: x["power"])["power"]
+    powers = [mage["power"] for mage in mages]
+    avg_power = sum(powers)/len(mages)
+    return {
+        "max_power": max_power,
+        "min_power": min_power,
+        "avg_power": float(f"{avg_power:.2f}")
+    }
+
+if __name__ == "__main__":
+    mages = [
+            {"name": "Aoris", "power": 41, "type": "Red"},
+            {"name": "Boris", "power": 1789, "type": "Communist"},
+            {"name": "Coris", "power": 1984, "type": "Faucil"},
+            {"name": "Doris", "power": 68, "type": "Hammer"},
+            {"name": "Eoris", "power": 1917, "type": "LittleRedBook"}
+                ]
+    spells = [
+        "FireBall",
+        "Ice Shards",
+        "Lightning Bolt"
+                ]
+    print(artifact_sorter(mages))
+    print(power_filter(mages, 1000))
+    print(spell_tranformer(spells))
+    print(mage_stats(mages))
